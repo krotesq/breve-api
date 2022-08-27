e59d59fcce02404203c8d5b3b521dd08a8043394
Noah <n.scholziii@gmail.com>
2022-08-28 00:48:30 +0200

    Update short.route.js

--- src/routes/short.route.js
+++ src/routes/short.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 // controller
 const { get, post } = require('../controller/short.controller');
 // middleware
-const checkurl = require('../middleware/checklink.middleware');
+const checkurl = require('../middleware/checkurl.middleware');
 
 router.get('/', get);
 router.post('/', checkurl, post);
