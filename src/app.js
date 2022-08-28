101341ee33ef3e81deb0c670586fd52a68d3a2de
aaronlyy <aaronlevican@gmail.com>
2022-08-28 20:42:37 +0200

    once mongodb msg

--- src/app.js
+++ src/app.js
@@ -26,6 +26,9 @@ app.use('*', routeAll);
 // database connection
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
+db.once('open', () => {
+    console.log('[+] MongoDB connected!');
+})
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // run
