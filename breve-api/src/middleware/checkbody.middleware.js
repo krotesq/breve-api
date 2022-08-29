b693c036d64d4005d80df269be5397392b3a05a4
aaronlyy <aaronlevican@gmail.com>
2022-08-29 08:34:01 +0200

    removed checkBody from route and fixed response import

--- breve-api/src/middleware/checkbody.middleware.js
+++ breve-api/src/middleware/checkbody.middleware.js
@@ -1,6 +1,6 @@
 // Middleware to check if a URL is passed with in the request body
 
-const {response} = require('../util/response.util');
+const response = require('../util/response.util');
 
 module.exports = (req, res, next) => {
     if(!req.body.url) {
--- breve-api/src/routes/short.route.js
+++ breve-api/src/routes/short.route.js
@@ -7,6 +7,6 @@ const checkUrl = require('../middleware/checkurl.middleware');
 const checkBody = require('../middleware/checkbody.middleware');
 
 router.get('/', get);
-router.post('/', checkBody, checkUrl, post);
+router.post('/', checkUrl, post); // fix checkBody
 
 module.exports = router;
