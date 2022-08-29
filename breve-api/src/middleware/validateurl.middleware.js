1a0bd3b52adcba4f951249b1abc03e88c81b954a
aaronlyy <aaronlevican@gmail.com>
2022-08-29 11:25:49 +0200

    validateurl update

--- breve-api/src/middleware/validateurl.middleware.js
+++ breve-api/src/middleware/validateurl.middleware.js
@@ -1,3 +1,9 @@
+const response = require('../util/response.util')
+const isURL = require('validator/lib/isURL')
+
 module.exports = (req, res, next) => {
+    if (!isURL(req.body.url)) {
+        return res.status(400).json(response(false, 'Wrong format'));
+    }
     next();
 }
--- breve-api/src/routes/short.route.js
+++ breve-api/src/routes/short.route.js
@@ -5,8 +5,9 @@ const { get, post } = require('../controller/short.controller');
 // middleware
 const checkUrl = require('../middleware/checkurl.middleware');
 const checkBody = require('../middleware/checkbody.middleware');
+const validateUrl = require('../middleware/validateurl.middleware');
 
 router.get('/', get);
-router.post('/', checkBody, checkUrl, post); // fix checkBody
+router.post('/', checkBody, validateUrl, checkUrl, post);
 
 module.exports = router;
