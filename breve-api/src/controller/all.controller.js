f38b0a3739add437f3055db2892c78f25fccdc0c
Noah <n.scholziii@gmail.com>
2022-09-01 11:20:11 +0200

    redirect to breve.club

--- breve-api/src/controller/all.controller.js
+++ breve-api/src/controller/all.controller.js
@@ -1,7 +1,8 @@
 const response = require('../util/response.util');
 
 const all = (req, res) => {
-    res.status(404).json(response(false, 'Not found!'));
+    res.redirect(process.env.DOMAIN);
+    //res.status(404).json(response(false, 'Not found!'));
 }
 
 module.exports = {all};
